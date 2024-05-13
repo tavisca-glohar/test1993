014c831386b1da54beb1b6b6b6197a4d4a85562e
tavisca-glohar <89198347+tavisca-glohar@users.noreply.github.com>
2024-05-13 12:16:36 +0530

    Delete .infra/iac/poc directory

--- .infra/iac/poc/provider.tf
+++
@@ -1,7 +0,0 @@
-
-provider "aws" {
-    region = "us-east-1"
-    default_tags {
-        tags = merge(var.mandatory_tags, { ResourceGroup = "${var.mandatory_tags.Product}-${var.mandatory_tags.AppName}"})
-    }
-}
--- .infra/iac/poc/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "aws_instance" {
-  type = any
-}
-
-
-variable "mandatory_tags" {
-    type = map(string)
-} 
